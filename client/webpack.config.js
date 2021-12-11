981a30b9d8fe5154d5a04c0a59d4d940c9758796
ktpi2000 <katapi2000@gmail.com>
2021-12-12 07:54:56 +0900

    css minify

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const webpack = require('webpack');
 
 const SRC_PATH = path.resolve(__dirname, './src');
@@ -59,6 +60,11 @@ const config = {
         ],
       },
     ],
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin()
+    ]
   },
   output: {
     filename: 'scripts/[name].js',
