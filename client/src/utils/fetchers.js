9a7292181dc49944c5ae3b8695105de8bba7e4e8
ktpi2000 <katapi2000@gmail.com>
2021-12-13 22:00:38 +0900

    jqueryをけしてみる

--- client/src/utils/fetchers.js
+++ client/src/utils/fetchers.js
@@ -1,18 +1,17 @@
-import { gzip } from 'pako';
+import {
+  gzip
+} from 'pako';
 
 /**
  * @param {string} url
  * @returns {Promise<ArrayBuffer>}
  */
 async function fetchBinary(url) {
-  const result = await $.ajax({
-    async: false,
-    dataType: 'binary',
-    method: 'GET',
-    responseType: 'arraybuffer',
-    url,
-  });
-  return result;
+  const result = await fetch(url, {
+    method: 'GET'
+  })
+  if (!result.ok) throw new Error()
+  return result.arrayBuffer()
 }
 
 /**
@@ -21,13 +20,11 @@ async function fetchBinary(url) {
  * @returns {Promise<T>}
  */
 async function fetchJSON(url) {
-  const result = await $.ajax({
-    async: false,
-    dataType: 'json',
-    method: 'GET',
-    url,
-  });
-  return result;
+  const result = await fetch(url, {
+    method: 'GET'
+  })
+  if (!result.ok) throw new Error()
+  return result.json()
 }
 
 /**
@@ -37,18 +34,14 @@ async function fetchJSON(url) {
  * @returns {Promise<T>}
  */
 async function sendFile(url, file) {
-  const result = await $.ajax({
-    async: false,
-    data: file,
-    dataType: 'json',
+  const result = await fetch(url, {
+    method: 'POST',
+    body: file,
     headers: {
       'Content-Type': 'application/octet-stream',
     },
-    method: 'POST',
-    processData: false,
-    url,
   });
-  return result;
+  return result.json();
 }
 
 /**
@@ -62,19 +55,20 @@ async function sendJSON(url, data) {
   const uint8Array = new TextEncoder().encode(jsonString);
   const compressed = gzip(uint8Array);
 
-  const result = await $.ajax({
-    async: false,
-    data: compressed,
-    dataType: 'json',
+  const result = await fetch(url, {
+    method: 'POST',
+    body: compressed,
     headers: {
       'Content-Encoding': 'gzip',
       'Content-Type': 'application/json',
     },
-    method: 'POST',
-    processData: false,
-    url,
   });
-  return result;
+  return result.json();
 }
 
-export { fetchBinary, fetchJSON, sendFile, sendJSON };
+export {
+  fetchBinary,
+  fetchJSON,
+  sendFile,
+  sendJSON
+};
